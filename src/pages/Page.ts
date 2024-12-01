326d3996c68d4bb8a62fc70636617f3af92ec0c4
John_Kick <Andre010400@web.de>
2024-12-01 16:35:21 +0100

    Improves template

--- src/pages/Page.ts
+++ src/pages/Page.ts
@@ -36,7 +36,7 @@ export default abstract class Page implements Renderable {
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 			<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
-			<link rel="stylesheet" href="http://localhost:${config.APP_PORT}/styles.css">
+			<link rel="stylesheet" href="/styles.css">
 				<title>Server Kicker</title>
 			</head>
 			<body data-bs-theme="dark">
